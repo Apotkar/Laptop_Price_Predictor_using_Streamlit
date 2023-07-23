f0ac39bf751194d94da944f43df1b8fece3c440a
Apotkar <ajinkya.potkar4@gmail.com>
2023-07-23 09:58:04 +0530

    main

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle
+import dill
 
-preprocessor=pickle.load(open('Streamlit_App\preprocessor.pkl','rb'))
-model=pickle.load(open('Streamlit_App\model.pkl','rb'))
+preprocessor=dill.load(open('Streamlit_App\preprocessor.pkl','rb'))
+model=dill.load(open('Streamlit_App\model.pkl','rb'))
 
 df=pd.read_csv(r'Streamlit_App\raw_data.csv')
 
